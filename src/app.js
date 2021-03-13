ac0a3973b3ab8821e109b4ac0344a648e2ed34e0
Lunaticfrg0 <emilio3scano@gmail.com>
2021-03-13 18:30:41 -0400

    add post reques to add to database

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+ app.use(bodyParser.json());
+  
+
 
 app.get("/", (req, res) => {
     
@@ -14,7 +20,7 @@ app.get("/", (req, res) => {
         }
         console.log("Connected to database")
         const db = client.db("user-account")
-        db.collection('users').findOne({name : "Emilio Escano"}, (err, result) => {
+        db.collection('users').findOne({name : "Katherine Escano"}, (err, result) => {
             if(err){
                 client.close()
                 return res.send("Something went wrong")
@@ -43,6 +49,27 @@ app.get("/users", (req, res) => {
         })
     }) 
 });
+
+app.post('/users', (req, res)=> {
+    const doc = {
+        name: req.body.name
+    }
+    MongoClient.connect("mongodb://admin:password@localhost:27017", {useNewUrlParser: true, useUnifiedTopology: true}, (error, client) =>{
+        if(error){
+            return console.log("Unable to connect to database")
+        }
+        console.log("Connected to database")
+        const db = client.db("user-account")
+        db.collection('users').insertOne(doc, (err, result) => {
+            if(err){
+                client.close()
+                return res.send("Something went wrong")
+            }
+            client.close()
+            res.send(result.ops[0])
+        })
+    }) 
+})
 
 app.listen(port, ()=>{
     console.log("Server is up on port " + port)
